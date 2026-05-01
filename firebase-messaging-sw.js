164ce7ac0f9cb7a36f3c68eb129549ea2e79d931
Tarek Tantawy <22655388+nasr-realestate@users.noreply.github.com>
2026-05-01 23:46:03 +0300

    Update Firebase initialization and notification handling

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,15 +1,23 @@
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
+// تهيئة Firebase داخل Service Worker
 firebase.initializeApp({
-  messagingSenderId: "733807203688"
+  apiKey: "AIzaSyBZxGDQ-N-Sue4GdoAmCfoUjeRkmAbdCZU",
+  authDomain: "simsartalabak.firebaseapp.com",
+  projectId: "simsartalabak",
+  storageBucket: "simsartalabak.firebasestorage.app",
+  messagingSenderId: "733807203688",
+  appId: "1:733807203688:web:a0582925001409994e32dd"
 });
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  self.registration.showNotification(payload.notification.title, {
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
     body: payload.notification.body,
-    icon: 'https://nasr-realestate.github.io/logo.png'
-  });
+    icon: 'https://nasr-realestate.github.io/logo.png',
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
