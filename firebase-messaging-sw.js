c92af0bb2e1426a5c33c4072e87a3b03c9a4b9a6
Tarek Tantawy <22655388+nasr-realestate@users.noreply.github.com>
2026-05-01 23:37:19 +0300

    Add Firebase messaging service worker for notifications

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,15 @@
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  messagingSenderId: "733807203688"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: 'https://nasr-realestate.github.io/logo.png'
+  });
+});
